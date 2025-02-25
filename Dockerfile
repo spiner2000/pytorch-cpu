7426f4021774d285e9b3d3d9727287ac056a031a
spiner2000 <1374700+spiner2000@users.noreply.github.com>
2025-02-25 07:25:17 +0000

    v0.2

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 # Dockerfile
 FROM python:3.11-slim as builder
-RUN pip install transformers torch --target=/deps
+RUN pip install  transformers
+RUN pip install  torch==2.6.0+cpu --target=/deps --index-url https://download.pytorch.org/whl/cpu
 
 FROM python:3.11-slim
+RUN apt update && apt install python3.11-dev -y 
 COPY --from=builder /deps /usr/local/lib/python3.11/site-packages
--- build-and-push.sh
+++ build-and-push.sh
@@ -18,10 +18,10 @@ if [ -z "$VERSION" ]; then
 fi
 
 # Устанавливаем имя образа
-IMAGE_NAME="sc-docker-registry.io:30500/datalens-us"
+IMAGE_NAME="sc-docker-registry.io:30500/docker-pytorch"
 echo "Строим образ: $IMAGE_NAME:$VERSION"
 # Строим Docker-образ с указанным тегом и latest
-docker build  -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest -f patch-us/Dockerfile .
+docker build  -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest .
 
 
 # Проверяем успешность последнего билда
